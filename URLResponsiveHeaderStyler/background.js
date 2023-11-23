30317461a4eb423eb857e50ce7b5fe3843ba7d77
spica1017 <spica@tetora.me>
2023-11-23 22:06:43 +0900

    日本語に変更・コメントを追加

--- URLResponsiveHeaderStyler/background.js
+++ URLResponsiveHeaderStyler/background.js
@@ -1,8 +1,13 @@
+// メッセージリスナーを追加
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
+    // メッセージがURLをチェックするものかどうかを確認する
     if (request.message === "check_url") {
+      // 送信者のタブのURLを取得
       let url = sender.tab.url;
+      // URLがリクエストの文字列を含むかどうかを確認
       if (url.includes(request.string)) {
+        // 色を変更するメッセージを送信者のタブに送信
         chrome.tabs.sendMessage(sender.tab.id, {
           message: "change_color"
         });
--- URLResponsiveHeaderStyler/popup.js
+++ URLResponsiveHeaderStyler/popup.js
@@ -1,8 +1,14 @@
+// DOMが完全に読み込まれた後に実行されるイベントリスナーを追加します
 document.addEventListener('DOMContentLoaded', function() {
+    // 'checkButton'というIDを持つ要素を取得します
     let checkButton = document.getElementById('checkButton');
+    // 'checkButton'にクリックイベントリスナーを追加します
     checkButton.addEventListener('click', function() {
+        // 'stringInput'というIDを持つ要素の値を取得します
         let stringInput = document.getElementById('stringInput').value;
+        // 現在のウィンドウでアクティブなタブをクエリします
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            // 'check_url'メッセージをバックグラウンドスクリプトに送信します
             chrome.runtime.sendMessage({
                 message: "check_url",
                 string: stringInput,
